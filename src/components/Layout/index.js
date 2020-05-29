81dfe6a63226bd205e71ac720ebd3467a91f328c
choco <chocoh14t@gmail.com>
2020-05-30 03:31:50 +0900

    ファイルパスの修正

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-import Header from '../header/index'
-import Footer from '../footer/index'
+import Header from '../header'
+import Footer from '../footer'
 import '../../styles/index.scss'
 import layoutStyles from './layout.module.scss'
 
