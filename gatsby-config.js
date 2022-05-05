7c2bfbe33d1eebd1cf4f06f333ffbdaa5e24fdac
choco <chocoh14t@gmail.com>
2022-05-05 16:24:56 +0900

    post(new): yonda-1
    add figcaption stytle

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,8 @@ module.exports = {
             resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 500,
-              linkImagesToOriginal: false
+              linkImagesToOriginal: false,
+              showCaptions: ['title', 'alt'],
             }
           },
           {
