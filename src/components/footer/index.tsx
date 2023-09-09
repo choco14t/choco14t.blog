6d42981a63244cfbf5579a6c1b29743e007b966b
choco <8362391+choco14t@users.noreply.github.com>
2023-09-10 03:03:47 +0900

    style: add GA notice

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -16,15 +16,20 @@ const Footer = () => {
 
   return (
     <footer className={footerStyles.footer}>
-      <div className={footerStyles.inner}>
-        <div>
-          <address>
-            © {data.site.siteMetadata.author} All rights reserved.
-          </address>
+      <div className={footerStyles.container}>
+        <div className={footerStyles.inner}>
+          <address>© {data.site.siteMetadata.author} All rights reserved.</address>
+          <div>
+            <Link to="/rss.xml"><FaRss /></Link>
+            <a href="https://github.com/choco14t" target="_blank" rel="noopener noreferrer"><FaGithub/></a>
+          </div>
         </div>
-        <div>
-          <Link to="/rss.xml"><FaRss /></Link>
-          <a href="https://github.com/choco14t" target="_blank" rel="noopener noreferrer"><FaGithub/></a>
+
+        <div className={footerStyles.ga}>
+          <p>
+            当サイトでは Google Analytics を使用しています。
+            詳細は <a href='https://policies.google.com/technologies/partner-sites?hl=ja'>Google のサービスを使用するサイトやアプリから収集した情報の Google による使用 – ポリシーと規約</a> をご覧ください。
+          </p>
         </div>
       </div>
     </footer>
