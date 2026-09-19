316aecb05c54d69a15a8c06b788b105796f98247
choco <8362391+choco14t@users.noreply.github.com>
2026-09-19 15:04:07 +0900

    feat: render Markdown soft line breaks

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
+import { unified } from '@astrojs/markdown-remark';
+import remarkBreaks from 'remark-breaks';
 
 export default defineConfig({
   site: 'https://blog.choco14t.net',
   trailingSlash: 'always',
   build: { inlineStylesheets: 'never' },
   image: { service: { entrypoint: 'astro/assets/services/noop' } },
-  markdown: { shikiConfig: { theme: 'nord' } },
+  markdown: { processor: unified({ remarkPlugins: [remarkBreaks] }), shikiConfig: { theme: 'nord' } },
 });
